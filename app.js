f117282b7b956f08b3b6ff0439f6b9c269da7ecb
Kaviya-Nagaraju <nkaviyanagaraju@gmail.com>
2024-06-01 12:53:50 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+const cors = require('cors');
+
+
+app.use(cors());
+// Middleware function to log requests
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+app.get('/api/message', (req, res) => {
+  res.json({ message: 'Hello from Express!' });
+});
+// Import routes
+const indexRouter = require('./routes/index');
+const aboutRouter = require('./routes/about');
+
+// Use routes
+app.use('/', indexRouter);
+app.use('/', aboutRouter);
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
